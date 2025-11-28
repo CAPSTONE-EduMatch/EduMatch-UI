cfd89611399625139191c8e5104ba40ad97428a5
notlongfen <phannguyenhoanglong@gmail.com>
2025-11-28 11:03:49 +0700

    chore(admin): refine ui and let admin change price, id

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
--- src/hooks/admin/index.ts
+++ src/hooks/admin/index.ts
@@ -1,2 +1,4 @@
+export { PLAN_TYPE, useAdminPlans } from "./useAdminPlans";
+export type { Plan, PlanUpdatePayload } from "./useAdminPlans";
 export { useAdminPostManagement } from "./useAdminPostManagement";
 export { useAdminUserManagement } from "./useAdminUserManagement";
